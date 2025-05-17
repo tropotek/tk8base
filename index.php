deb39c9a9fe668cce41f13db79c38e6851b48531
tropotek <info@tropotek.com>
2025-05-18 07:02:43 +1000

    ~Auto: Commit

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 
 try {
     require_once __DIR__ . '/_prepend.php';
-
     $response = \Bs\Factory::instance()->getFrontController()->handle(\Bs\Factory::instance()->getRequest());
     $response->send();
     \Bs\Factory::instance()->getFrontController()->terminate(\Bs\Factory::instance()->getRequest(), $response);
