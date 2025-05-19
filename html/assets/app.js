70c38fc427d149d189e6f818f344ca1481aa6f23
tropotek <info@tropotek.com>
2025-05-19 13:18:54 +1000

    ~Auto: Commit

--- html/assets/app.js
+++ html/assets/app.js
@@ -3,34 +3,49 @@
  */
 
 jQuery(function ($) {
-  // Init page javascript functions
-  tkbase.initDialogConfirm();
-  tkbase.initTkInputLock();
-  tkbase.initDataToggle();
-  tkbase.initTinymce();
-  tkbase.initTkFormTabs();
-  tkbase.initDatepicker();
-  tkbase.initPasswordToggle();
-  tkbase.initHtmxConfirmDialog();
+    // Init page javascript functions
+    tkbase.initDialogConfirm();
+    tkbase.initTkInputLock();
+    tkbase.initDataToggle();
+    tkbase.initTinymce();
+    tkbase.initTkFormTabs();
+    tkbase.initDatepicker();
+    tkbase.initPasswordToggle();
+    tkbase.initHtmxConfirmDialog();
 
-  // app.initHtmxToasts();
+    // app.initHtmxToasts();
+    app.initNotifications();
 });
 
 let app = function () {
-  "use strict";
+    "use strict";
 
-  // let initHtmxToasts = function () {
-  //   // Enable HTMX logging in the console
-  //   //htmx.logAll();
-  //   // Trigger on finished request loads (ie: after a form submits)
-  //   $(document).on('htmx:afterSettle', '.toastPanel', function () {
-  //     $('.toast', this).toast('show');
-  //   });
-  // };
+    // let initHtmxToasts = function () {
+    //   // Enable HTMX logging in the console
+    //   //htmx.logAll();
+    //   // Trigger on finished request loads (ie: after a form submits)
+    //   $(document).on('htmx:afterSettle', '.toastPanel', function () {
+    //     $('.toast', this).toast('show');
+    //   });
+    // };
+
+    let initNotifications = function () {
+        if (typeof Notification === 'undefined') return;
+
+        if (Notification.permission !== 'granted') {
+            let promise = Notification.requestPermission();
+            promise.then(function () {
+                if (Notification.permission === 'granted') {
+                    $(document).trigger('notify:reload');
+                }
+            });
+        }
+    };
 
 
-  return {
-    // initHtmxToasts: initHtmxToasts
-  }
+    return {
+        // initHtmxToasts: initHtmxToasts,
+        initNotifications: initNotifications,
+    }
 
 }();
