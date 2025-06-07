13790a84ecee0c3521bd68497a1e4b3205129bf1
tropotek <info@tropotek.com>
2025-06-08 07:43:58 +1000

    ~Auto: Commit

--- src/App/Component/Test.php
+++ src/App/Component/Test.php
@@ -38,26 +38,38 @@ class Test extends \Dom\Renderer\Renderer implements \Dom\Renderer\DisplayInterf
     {
         $html = <<<HTML
 <div id="" var="container">
-  <div class="card mb-3">
-    <div class="card-header"><i class="fas fa-cogs"></i> <span var="title"></span></div>
-    <div class="card-body" var="content" id="test-content">
-        <p>
-            <button type="button" class="btn btn-outline-primary" var="btn1"
-                hx-get="/component/test"
-                hx-target="#test-content"
-                hx-select="#test-content"
-                hx-swap="outerHTML">Reload</button>
-        </p>
-        <p>Test Component ... [<span var="time"></span>]</p>
+    <div class="card mb-3">
+        <div class="card-header"><i class="fas fa-cogs"></i> <span var="title"></span></div>
+        <div class="card-body" var="content" id="test-content">
+            <p>
+                <button type="button" class="btn btn-outline-primary" var="btn1"
+                    hx-get="/component/test"
+                    hx-target="#test-content"
+                    hx-select="#test-content"
+                    hx-swap="outerHTML">Reload</button>
+            </p>
+            <p>Test Component ... [<span var="time"></span>]</p>
+        </div>
     </div>
-  </div>
 <script>
-    jQuery(function($) {
-        const container = '#{$this->getContainerId()}';
+jQuery(function($) {
+    const container = '#{$this->getContainerId()}';
 
-        $('.card-body', container).append('<p>Javascript Text</p>');
 
+    function init() {
+        // todo
+    }
+
+    $(document).on('htmx:afterSettle', container, function(e) {
+        // init component on reload
+        init();
     });
+
+    init();
+
+    $('.card-body', container).append('<p>Javascript Text</p>');
+
+});
 </script>
 <style>
 .test-com .card-header {
