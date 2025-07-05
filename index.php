cf8758d34f0a0706c41f3df6f7baeba4d15b80b4
tropotek <info@tropotek.com>
2025-07-06 05:15:04 +1000

    ~Auto: Commit

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ try {
     require_once __DIR__ . '/_prepend.php';
     defined('TKAPP') || die();
 
-    $response = \Bs\Factory::instance()->getFrontController()->handle(\Bs\Factory::instance()->getRequest());
+    $factory  = \App\Factory::instance();
+    $response = $factory->getFrontController()->handle($factory->getRequest());
     $response->send();
-    \Bs\Factory::instance()->getFrontController()->terminate(\Bs\Factory::instance()->getRequest(), $response);
-
+    $factory->getFrontController()->terminate($factory->getRequest(), $response);
 } catch (\Exception $e) {
     error_log($e->__toString());
 }
