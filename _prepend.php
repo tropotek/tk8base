8551572ce51f74d5728f2ba09394b115cba340c7
tropotek <info@tropotek.com>
2025-07-06 04:33:09 +1000

    ~Auto: Commit

--- _prepend.php
+++ _prepend.php
@@ -5,10 +5,9 @@
  * Load this file when running any script to
  * set up and bootstrap the system environment
  */
-
-defined('TKAPP') || die();
-
 $composer = include __DIR__ . '/vendor/autoload.php';
+
+define('TKAPP', true);
 
 // Init Tk System Objects
 $config  = \Tk\Config::instance();
--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 /*
  * @author Tropotek <http://www.tropotek.com/>
  */
-define('TKAPP', true);
 
 try {
     require_once __DIR__ . '/_prepend.php';
+    defined('TKAPP') || die();
+
     $response = \Bs\Factory::instance()->getFrontController()->handle(\Bs\Factory::instance()->getRequest());
     $response->send();
     \Bs\Factory::instance()->getFrontController()->terminate(\Bs\Factory::instance()->getRequest(), $response);
+
 } catch (\Exception $e) {
     error_log($e->__toString());
 }
